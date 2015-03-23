65861e21ba00d834017f635d4f3061b4dbd5088d
nowsilence <nbperson@163.com>
2015-03-23 15:54:04 +0800

    Update build_libcurl_dist.sh

--- build_libcurl_dist.sh
+++ build_libcurl_dist.sh
@@ -16,8 +16,8 @@ function build_for_arch() {
 
 TMP_DIR=/tmp/build_libcurl_$$
 
-build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.0.sdk ${TMP_DIR}/i386 || exit 1
-build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.0.sdk ${TMP_DIR}/x86_64 || exit 2
+build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.1.sdk ${TMP_DIR}/i386 || exit 1
+build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.1.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.1.sdk ${TMP_DIR}/arm64 || exit 3
 build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.1.sdk ${TMP_DIR}/armv7s || exit 4
 build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.1.sdk ${TMP_DIR}/armv7 || exit 5
